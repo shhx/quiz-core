50da91d04e25828fd25405d34ace9e4f37f2e0ed
shhx <luisgomezlo.lg@gmail.com>
2016-04-23 16:53:28 +0200

    Basic author page

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser');
 var partials = require('express-partials');
 
 var routes = require('./routes/index');
+var author = require('./routes/author');
+
 
 var app = express();
 
@@ -25,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(partials());
 
 app.use('/', routes);
+app.use('/author', author);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller.js');
-/* GET home page. */
+//Home page
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+//Question pages
 router.get('/question', quizController.question);
 router.get('/check', quizController.check);
+
+// Author page
+router.get('/author', function (req, res, next) {
+	res.render('author', { title: 'Página de autor' , author: {name: 'Luis Alberto Gomez Lopez', photo: "public/mia.jpg"}});
+});
 
 module.exports = router;
