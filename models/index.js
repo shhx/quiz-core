bc3c8567941239f4f91462495f3af29858d3d6d3
shhx <luisgomezlo.lg@gmail.com>
2016-05-01 02:09:26 +0200

    Despligue DB en Heroku.

--- models/index.js
+++ models/index.js
@@ -2,7 +2,16 @@ var path = require('path');
 
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize(null, null, null, {dialect: "sqlite", storage: "quiz.sqlite"});
+var url, storage;
+if (!process.env.DATABASE_URL) {
+	url = "sqlite:///";
+	storage = "quiz.sqlite";
+} else {
+	url = process.env.DATABASE_URL;
+	storage = process.env.DATABASE_STORAGE || "";
+}
+
+var sequelize = new Sequelize(url, {storage: storage, omitNull: true});
 
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
