ba94ae44d04b0aafffe1a36e79afb1067d246aab
shhx <luisgomezlo.lg@gmail.com>
2016-05-08 19:20:49 +0200

    Validate inputs.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,5 @@
 var models = require('../models')
-
+var Sequelize = require('sequelize');
 
 // Autoload :quizId
 exports.load = function(req, res, next, quizId){
@@ -65,6 +65,13 @@ exports.create = function(req, res, next) {
 		req.flash('success', 'Quiz creado con exito');
 	    res.redirect('/quizzes');  // res.redirect: Redirección HTTP a lista de preguntas
 	    })
+	.catch(Sequelize.ValidationError, function(error){
+		req.flash('error', 'Errores en el formulario:');
+		for (var i in error.errors) {
+			req.flash('error', error.errors[i].value);
+		}
+		res.render('quizzes/new', {quiz: quiz});
+	})
 	.catch(function(error) {
 		req.flash('error', 'Error al crear un Quiz: '+error.message);
 		next(error);
--- models/quiz.js
+++ models/quiz.js
@@ -1,3 +1,10 @@
 module.exports = function (sequelize, DataTypes) {
-	return sequelize.define('Quiz', {question: DataTypes.STRING, answer: DataTypes.STRING});
-}
+	return sequelize.define('Quiz', 
+		{question: {type: DataTypes.STRING, 
+					validate: {notEmpty: {msg: "Falta Pregunta"}}
+					},
+		answer: {type: DataTypes.STRING, 
+					validate: {notEmpty: {msg: "Falta Respuesta"}}
+				}
+		});
+};
