16b164c4010c3d5b7bb57b8216cb8a05b029de66
shhx <luisgomezlo.lg@gmail.com>
2016-05-19 00:05:04 +0200

    Users added

--- models/index.js
+++ models/index.js
@@ -19,9 +19,13 @@ var Quiz = sequelize.import(path.join(__dirname,'quiz'));
 // Importar la definicion de la tabla Comments de comment.js
 var Comment = sequelize.import(path.join(__dirname,'comment'));
 
+// Importar la definicion de la tabla Users de user.js
+var User = sequelize.import(path.join(__dirname,'user'));
+
 // Relaciones entre modelos
 Comment.belongsTo(Quiz);
 Quiz.hasMany(Comment);
 
 exports.Quiz = Quiz;
 exports.Comment = Comment;
+exports.User = User;
